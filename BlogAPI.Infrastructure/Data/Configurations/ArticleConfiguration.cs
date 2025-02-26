133eaf0ec21fe10cd7e56266ccd99251367bf960
Matheus Wisch <wisch.pg@outlook.com>
2025-02-26 14:32:31 -0300

    :sparkles: Feat: Create configurations and add entity framework core

---
+++ BlogAPI.Infrastructure/Data/Configurations/ArticleConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using BlogApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogAPI.Infrastructure.Data.Configurations;
+
+public class ArticleConfiguration : IEntityTypeConfiguration<Article>
+{
+    public void Configure(EntityTypeBuilder<Article> builder)
+    {
+        builder.HasKey(a => a.Id);
+            
+        builder.Property(a => a.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+            
+        builder.Property(a => a.Content)
+            .IsRequired();
+            
+        builder.Property(a => a.CreatedAt)
+            .IsRequired();
+            
+        builder.HasOne(a => a.Author)
+            .WithMany(u => u.Articles)
+            .HasForeignKey(a => a.AuthorId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
