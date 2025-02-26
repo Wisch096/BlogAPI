2dbda5555a0b1518fe9438c6c8eb696f3e3f61a1
Matheus Wisch <wisch.pg@outlook.com>
2025-02-26 14:01:12 -0300

    :sparkles: Feat: Creating article use case interface

---
+++ BlogAPI.Application/Interfaces/IArticleUseCase.cs
@@ -0,0 +1,10 @@
+﻿using BlogApi.Application.DTOs;
+
+namespace BlogApi.Application.Interfaces;
+
+public interface IArticleUseCase
+{
+    Task<ArticleDTO> CreateArticleAsync(CreateArticleDTO articleDTO, Guid userId);
+    Task<IEnumerable<ArticleDTO>> GetAllArticlesAsync();
+    Task<ArticleDTO> GetArticleByIdAsync(Guid id);
+}
